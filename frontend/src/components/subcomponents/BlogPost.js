966ff3e4e12f63ab243c0b882bb039df4810b7f3
laurenah <lozhambo@gmail.com>
2020-10-05 16:58:18 +1100

    add shadow to nav on home, remove shadow from blog cards

--- frontend/src/components/subcomponents/BlogPost.js
+++ frontend/src/components/subcomponents/BlogPost.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../../blog.css';
+import { ReactComponent as Logo } from '../../laurenah_logo-08.svg';
 
 // BlogPost component
 class BlogPost extends React.Component {
@@ -16,17 +17,54 @@ class BlogPost extends React.Component {
 
     render() {
         return (
-            <div className='entry'>
-                <div className='title'>
-                    <h2>{this.state.title}</h2>
-                    <span className='byline'>{new Date(this.state.created).toLocaleDateString('en-US', {
-                        month: 'short',
-                        day: '2-digit',
-                        year: 'numeric'
-                    })}</span>
+            <div>
+                <div className='small-nav'>
+                    <Logo className='monstera logo-sm'/>
+                    <ul>
+                        <li><a href='/'>Home</a></li>
+                        <li><a href='/blog'>Blog</a></li>
+                        <li><a href='/contact'>Contact</a></li> 
+                    </ul>
                 </div>
-                <p>{this.state.content}</p>
-            </div> 
+                <div className='single-entry'>
+                    <div className='title'>
+                        <h2>{this.state.title}</h2>
+                        <span className='byline'>{new Date(this.state.created).toLocaleDateString('en-US', {
+                            month: 'short',
+                            day: '2-digit',
+                            year: 'numeric'
+                        })}</span>
+                    </div>
+                    <p dangerouslySetInnerHTML={{__html: this.state.content}}></p>
+                </div> 
+
+                <div className='footer' style={{
+                    'width': '100%',
+                    'backgroundColor': '#fff',
+                    'padding': '20px',
+                    'position': 'absolute',
+                    'bottom': '0',
+                    'color': 'gray'
+                }}>
+                    <ul>
+                        <li style={{'display': 'inline', 'padding': '10px'}}>lauren hammond © 2020</li>
+                        <li style={{'display': 'inline', 'padding': '10px'}}>
+                            <a href='https://github.com/laurenah'
+                            style={{
+                                'textDecoration': 'none',
+                                'color': 'gray'
+                            }}>Github</a>
+                        </li>
+                        <li style={{'display': 'inline', 'padding': '10px'}}>
+                            <a href='https://www.linkedin.com/in/lauren-h-81917ab4/'
+                            style={{
+                                'textDecoration': 'none',
+                                'color': 'gray'
+                            }}>Linkedin</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
         );
     }
 }
